a92da8adf415e1c30afa5ac0edab7ca1b41b78da
malika0301 <kr.serdce261086@mail.ru>
2026-01-06 01:13:25 +0500

    123

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -37,13 +37,19 @@ const HomePage = () => {
 
   function decrease(id){
     setCart((prev) => {
-      return prev.map((el) => {
-        if (el.id === id) {
-          return { ...el, qty: el.qty - 1 }
-        } else {
-          return el
-        }
-      })
+      let cartItem = prev.find((el) => el.id === id);
+      if(cartItem.qty > 1){
+        return prev.map((el) => {
+          if (el)
+            if (el.id === id) {
+              return { ...el, qty: el.qty - 1 }
+            } else {
+              return el
+            }
+        })
+      }else{
+         return prev.filter((el) => el.id !== id)
+      }
     })
   }
 
